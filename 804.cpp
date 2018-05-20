6b37a2a744e8b3306337a12c6fc07abce9eeb663
darcysail <darcysail@gmail.com>
2018-05-20 11:56:23 +0800

    add 804 Solution.
    Signed-off-by: darcysail <darcysail@gmail.com>

---
+++ 804.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string str_arr[] = {".-",   "-...", "-.-.", "-..",  ".",    "..-.", "--.",
+                    "....", "..",   ".---", "-.-",  ".-..", "--",   "-.",
+                    "---",  ".--.", "--.-", ".-.",  "...",  "-",    "..-",
+                    "...-", ".--",  "-..-", "-.--", "--.."};
+
+class Solution
+{
+  public:
+    int uniqueMorseRepresentations(vector<string> &words)
+    {
+        map<string, bool> result;
+        for (int i = 0; i < words.size(); ++i) {
+            string code;
+            for (int j = 0; j < words[i].length(); ++j) {
+                code += str_arr[words[i][j] - 'a'];
+            }
+            result[code] = true;
+        }
+        return result.size();
+    }
+};
+
+int main() { Solution sol; }
