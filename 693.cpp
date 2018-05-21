8564d896e9c1233ed59127fd392ed38a3b56de45
darcysail <darcysail@gmail.com>
2018-05-21 13:51:37 +0800

    add 693 Solution.
    Signed-off-by: darcysail <darcysail@gmail.com>

---
+++ 693.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    bool hasAlternatingBits(int n)
+    {
+        int low_bit = !(n & 1);
+        while (n) {
+            if (!(n & 1 ^ low_bit)) {
+                return false;
+            }
+            low_bit = n & 1;
+            n >>= 1;
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    cout << sol.hasAlternatingBits(5) << endl;
+    cout << sol.hasAlternatingBits(7) << endl;
+    cout << sol.hasAlternatingBits(11) << endl;
+    cout << sol.hasAlternatingBits(10) << endl;
+}
