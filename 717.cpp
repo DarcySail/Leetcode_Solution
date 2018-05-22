0d88627e6a07d7a60704e91a499aa86aa09cd84d
darcysail <darcysail@gmail.com>
2018-05-22 10:59:52 +0800

    add 717 Solution.
    Signed-off-by: darcysail <darcysail@gmail.com>

---
+++ 717.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    bool isOneBitCharacter(vector<int> &bits)
+    {
+        int arr[2][2] = {{0, 1}, {0, 0}};
+        int len = bits.size();
+        int pos = 0;
+        for (int i = 0; i < len - 1; ++i) {
+            pos = arr[pos][bits[i]];
+        }
+        return pos == 0;
+    }
+};
+
+int main() { Solution sol; }
