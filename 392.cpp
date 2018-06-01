6970d54cf4b76266f7539d79411c8186f2bbaef9
darcysail <darcysail@gmail.com>
2018-06-01 13:22:47 +0800

    add 392 Solution.
    Signed-off-by: darcysail <darcysail@gmail.com>

--- 392.cpp
+++ 392.cpp
@@ -1,14 +1,32 @@
-class Solution {
-public:
-    bool isSubsequence(string s, string t) {
-		int index = 0;
-		for(int i = 0; i < t.length(); ++i)
-		{
-			if(t[i] == s[index])
-			{
-				++index;
-			}
-		}
-		return s.length() == index;
+#include <algorithm>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+  public:
+    bool isSubsequence(string s, string t)
+    {
+        int i, j;
+        for (i = 0, j = 0; i < t.length() && j < s.length(); ++i) {
+            if (s[j] == t[i]) {
+                ++j;
+            }
+        }
+        return s.length() == j;
     }
 };
+
+int main()
+{
+    Solution sol;
+    sol.isSubsequence("abc", "ahbgdc");
+}
