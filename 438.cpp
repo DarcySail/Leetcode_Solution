538fb9111e257cb40a1b1df0aeefbad20e945ca0
darcysail <darcysail@gmail.com>
2018-07-01 23:05:40 +0800

    add 438 Solution.
    Signed-off-by: darcysail <darcysail@gmail.com>

--- 438.cpp
+++ 438.cpp
@@ -14,16 +14,51 @@ class Solution
   public:
     vector<int> findAnagrams(string s, string p)
     {
-        int dict[128] = {0};
-        int lens = s.length(), lenp = p.length();
-        vector<int> dp(s.length(), 0);
+        vector<int> result;
+        if (s.length() == 0)
+            return result;
 
-        return NULL;
+        vector<int> dict(256, 0);
+        int win_size = p.length();
+        for (auto i : p)
+            ++dict[i];
+        for (size_t i = 0; i < p.length(); ++i) {
+            if (dict[s[i]]-- > 0) {
+                --win_size;
+            }
+        }
+
+        if (win_size == 0) {
+            result.push_back(0);
+        }
+        for (size_t left = 0, right = p.length(); right < s.length();
+             ++left, ++right) {
+            if (++dict[s[left]] > 0) {
+                ++win_size;
+            }
+            if (dict[s[right]]-- > 0) {
+                --win_size;
+            }
+            if (win_size == 0) {
+                result.push_back(left + 1);
+            }
+        }
+        return result;
     }
 };
 
+void pv(vector<int> &arr)
+{
+    for (auto i : arr) {
+        cout << i << ' ';
+    }
+    cout << endl;
+}
 int main()
 {
     Solution sol;
-    sol.findAnagrams("abab", "ab");
+    vector<int> arr = sol.findAnagrams("abab", "ab");
+    pv(arr);
+    arr = sol.findAnagrams("cbaebabacd", "abc");
+    pv(arr);
 }
